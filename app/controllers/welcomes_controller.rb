451eacaa5f4fe495eb9421a90c53804b883d6615
jayzen <jayzen@foxmail.com>
2019-03-01 12:14:36 +0800

    do some fix

--- app/controllers/welcomes_controller.rb
+++ app/controllers/welcomes_controller.rb
@@ -6,6 +6,7 @@ class WelcomesController < ApplicationController
   def index
     @user = User.find_by(email: "demo@demo.com")
     @carousels = @user.carousels.where(reveal: true)
+    set_meta_tags(keywords: @user.basic.keywords)
   end
 
   def show

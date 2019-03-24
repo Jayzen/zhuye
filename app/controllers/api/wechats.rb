0b3fcb5c6bd8f8ba1bcbe821f5a4709c9eb3b510
jayzen <jayzen@foxmail.com>
2019-03-24 17:31:26 +0800

    do some fix

--- app/controllers/api/wechats.rb
+++ app/controllers/api/wechats.rb
@@ -5,7 +5,10 @@ module API
 
     get ':subdomain' do
       @user = User.find_by(subdomain: params[:subdomain])
-      {carousels: @user.carousels.where(reveal: true), services: @user.services.where(reveal: true)}
+      {carousels: @user.carousels.where(reveal: true), 
+       services: @user.services.where(reveal: true),
+       introduction: @user.introductions.where(reveal: true).first
+      }
     end
 
     get ':subdomain/services/:id' do
